1e4523dd6984ba255e5d1648e5a45d697875def0
James Villarrubia <james.villarrubia@gmail.com>
2024-09-12 16:46:12 -0400

    adds new os typing in the install script

--- build/install.sh
+++ build/install.sh
@@ -3,6 +3,30 @@
 INSTALL_DIR="/usr/local/bin"
 REPO="jamesvillarrubia/pullcraft"
 
+# Determine OS and architecture
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    OS="macos"
+elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    OS="ubuntu"
+else
+    echo "Unsupported operating system: $OSTYPE"
+    exit 1
+fi
+
+ARCH=$(uname -m)
+case $ARCH in
+    x86_64)
+        ARCH="x64"
+        ;;
+    arm64|aarch64)
+        ARCH="arm64"
+        ;;
+    *)
+        echo "Unsupported architecture: $ARCH"
+        exit 1
+        ;;
+esac
+
 # Get the latest release version
 LATEST_VERSION=$(curl -s https://api.github.com/repos/$REPO/releases/latest | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
 
@@ -12,10 +36,13 @@ if [ -z "$LATEST_VERSION" ]; then
 fi
 
 echo "Latest version: $LATEST_VERSION"
+echo "Detected OS: $OS, Architecture: $ARCH"
 
 # Download the latest release
-echo "Downloading PullCraft..."
-curl -L "https://github.com/$REPO/releases/download/$LATEST_VERSION/pullcraft" -o pullcraft
+BINARY_NAME="pullcraft-$OS"
+DOWNLOAD_URL="https://github.com/$REPO/releases/download/$LATEST_VERSION/$BINARY_NAME"
+echo "Downloading PullCraft from $DOWNLOAD_URL..."
+curl -L "$DOWNLOAD_URL" -o pullcraft
 
 if [ $? -ne 0 ]; then
     echo "Failed to download PullCraft. Please check your internet connection and try again."
